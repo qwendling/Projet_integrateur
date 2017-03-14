a661aeba9564e9952ed97f702adfdaf7b9409c3d
pcatalina <pavlovcatalina@gmail.com>
2017-03-14 23:52:00 +0100

    hud scene

---
+++ Unity Project - Leap Motion FPS/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    public float TotalHt;
+    public float CurrentHt;
+
+	// Use this for initialization
+	void Start () {
+        CurrentHt = TotalHt;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            GetDamage();
+        }
+	}
+
+    void GetDamage()
+    {
+        CurrentHt -= 5;
+        //transform.localScale = new Vector3((CurrentHt/TotalHt),1,1);
+        Debug.Log((CurrentHt / TotalHt));
+        Vector3 rowX = new Vector3(-28.5f, 0, 0);
+        transform.Translate(rowX);
+    }
+}
