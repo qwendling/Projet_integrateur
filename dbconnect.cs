39b069d7b3d81d2859f3b522bd590e81373de410
MrSaTurNiN <abegg.jp@hotmail.fr>
2017-03-15 16:20:32 +0100

    Base de donnée: update CLASSES
    Nouvelle méthode dans la class DBConnect pour faire un update sur la table CLASSES de la BDD.

--- dbconnect.cs
+++ dbconnect.cs
@@ -4,7 +4,6 @@ class DBConnect{
 	private string nomDeLaBase;
 	private string uid;
 	private string mdp;
-
 	public DBConnect(){
 		serveur = "localhost";
 		nomDeLaBase = "X";
@@ -29,8 +28,8 @@ class DBConnect{
 	 *  Initialisation du MySqlConnection
 	 */
 	private void init(){
-		string connectionString = "SERVEUR="+serveur+";
-			DATABASE="+nomDeLaBase+";UID="+uid+";PASSWORD="+mdp+";";
+		string connectionString = "SERVEUR="+serveur+";"+
+			+"DATABASE="+nomDeLaBase+";UID="+uid+";PASSWORD="+mdp+";";
 		connection = new MySqlConnection(connectionString);		
 	}
 
@@ -41,6 +40,19 @@ class DBConnect{
 	private bool ouvertureConnection(){
 		try{
 			connection.Open();
+			return true;
+		}catch(MySqlException e){
+			return false;
+		}
+	}
+
+	/**
+	 * Fermeture de la connection avec le serveur
+	 * retour: true si réussite, false si erreur
+	 */
+	private bool fermetureConnection(){
+		try{
+			connection.Cpen();
 			return true;
 		}catch(MySqlException e){
 			return false;
@@ -65,23 +77,52 @@ class DBConnect{
 		return false;
 		
 	}
+
+
 	/**
 	 * Insertion d'une nouvelle classe sur la base
 	 * arguments: les champs d'un n-uplets
 	 * retourn: true si réussite
 	 */
 	public bool insertClass(int idJoueuer,
-							string nomClass,
-							int id_armeA,
-							int id_armeB,
-							int id_armeC,
-							int id_avatar,
-							int rgb_avatar)
+				string nomClass,
+				int id_armeA,
+				int id_armeB,
+				int id_armeC,
+				int id_avatar,
+				int rgb_avatar)
 	{
 		string query = "INSERT INTO CLASSES(nom_classe,id_joueur,"
 		+"id_armeA,id_armeB,id_armeC,id_avatar,rgb_avatar)"+
 		+"VALUES('"+nomClass+"',"+id_armeA+","+id_armeB+","+id_armeC
 		+","+id_avatar+","+rgb_avatar+");";
+		if(this.ouvertureConnection() == true){
+			MySqlCommand cmd = new MySqlCommand(query,connection);
+			cmd.ExecuteNonQuery();
+			this.fermetureConnection();
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Mettre à jour une classe dans la base
+	 * arguments: champs d'un n-uplets
+	 * retourn: true si réussite
+	 */
+	 public bool updateClass(int idClass,
+				string nomClass,
+				int id_armeA,
+				int id_armeB,
+				int id_armeC,
+				int id_avatar,
+				int rgb_avatar)
+	{
+		string query ="UPDATE CLASSES"
+		+"SET id_armeA="+id_armeA+",id_armeB="+id_armeB+","
+		+"id_armeC="+id_armeC+",id_avatar="+id_avatar+","
+		+"nom_classe = '"+nomClass+"',"
+		+"rgb_avatar="+rgb_avatar+" WHERE id_classe = "+idClass+";";
 		if(this.ouvertureConnection() == true){
 			MySqlCommand cmd = new MySqlCommand(query,connection);
 			cmd.ExecuteNonQuery();
