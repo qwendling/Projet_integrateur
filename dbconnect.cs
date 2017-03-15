6325aad241edfe33c05d897314c51e240dd08e56
MrSaTurNiN <abegg.jp@hotmail.fr>
2017-03-15 15:02:33 +0100

    Insertion classes de joueur
    Nouvelle méthode dans l'objet DBConnect

--- dbconnect.cs
+++ dbconnect.cs
@@ -46,29 +46,16 @@ class DBConnect{
 			return false;
 		}
 	}
-
-	/**
-	 * Fermeture de la connection avec le serveur
-	 * retour: true si réussite, false si erreur
-	 */
-	private bool fermetureConnection(){
-		try{
-			connection.Cpen();
-			return true;
-		}catch(MySqlException e){
-			return false;
-		}
-	}
 	/**
 	 * Insertion d'un nouveau joueur sur la base
-	 * argument: nom, le nom du joueur; mdp: le mot de passe du joueur
+	 * arguments: nom: le nom du joueur; mdp: le mot de passe du joueur
 	 * retour: true si réussite
 	 */
 	public bool insertNouveauClient(string nom,
 					string mdp)
 	{
-		string query = "INSERT INTO JOUEURS(login_joueur,hash_mdp) 
-			VALUES ('"+nom+"','"+mdp+"');";
+		string query = "INSERT INTO JOUEURS(login_joueur,hash_mdp) "+
+		"VALUES ('"+nom+"','"+mdp+"');";
 		if(this.ouvertureConnection() == true){
 			MySqlCommand cmd = new MySqlCommand(query,connection);
 			cmd.ExecuteNonQuery();
@@ -78,5 +65,31 @@ class DBConnect{
 		return false;
 		
 	}
+	/**
+	 * Insertion d'une nouvelle classe sur la base
+	 * arguments: les champs d'un n-uplets
+	 * retourn: true si réussite
+	 */
+	public bool insertClass(int idJoueuer,
+							string nomClass,
+							int id_armeA,
+							int id_armeB,
+							int id_armeC,
+							int id_avatar,
+							int rgb_avatar)
+	{
+		string query = "INSERT INTO CLASSES(nom_classe,id_joueur,"
+		+"id_armeA,id_armeB,id_armeC,id_avatar,rgb_avatar)"+
+		+"VALUES('"+nomClass+"',"+id_armeA+","+id_armeB+","+id_armeC
+		+","+id_avatar+","+rgb_avatar+");";
+		if(this.ouvertureConnection() == true){
+			MySqlCommand cmd = new MySqlCommand(query,connection);
+			cmd.ExecuteNonQuery();
+			this.fermetureConnection();
+			return true;
+		}
+		return false;
+	}
+							
 
 }
