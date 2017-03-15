389ccf2194b9622c15387f6469f11e7547bfdf02
MrSaTurNiN <abegg.jp@hotmail.fr>
2017-03-15 14:07:41 +0100

    Base de données
    Le script pour créer la base de données.Le script était uniquement dispo sur le Phabricator

---
+++ bddfps.sql
@@ -0,0 +1,55 @@
+/** code mysql **/
+DROP TABLE IF EXISTS CLASSES;
+DROP TABLE IF EXISTS ARMES;
+DROP TABLE IF EXISTS JOUEURS;
+DROP TABLE IF EXISTS AVATARS;
+DROP TABLE IF EXISTS ARENES;
+
+
+
+/** tables **/
+CREATE TABLE ARMES(
+	id_arme int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nom_arme varchar(256),
+	cadence_arme decimal(5,3),
+	degats_arme int,
+	range_arme int
+)ENGINE = INNODB;
+
+CREATE TABLE JOUEURS(
+	id_joueur int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	login_joueur varchar(256),
+	hash_mdp varchar(256)
+)ENGINE = INNODB;
+
+
+CREATE TABLE AVATARS(
+	id_avatar int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nom_avatar varchar(256),
+	modele_avatar varchar(256)
+)ENGINE = INNODB;
+
+CREATE TABLE CLASSES(
+	id_classe int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nom_classe varchar(256),
+	id_joueur int,
+	id_armeA int,
+	id_armeB int,
+	id_armeC int,
+	id_avatar int,
+	rgb_avatar int
+)ENGINE = INNODB;
+
+/** Pas forcement necessaire **/
+CREATE TABLE ARENES(
+	id_arene int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	nom_arene varchar(256),
+	scene_arene varchar(256)
+)ENGINE = INNODB;
+
+/** contraintes **/
+ALTER TABLE CLASSES ADD CONSTRAINT fkJoueur FOREIGN KEY(id_joueur) REFERENCES JOUEURS(id_joueur);
+ALTER TABLE CLASSES ADD CONSTRAINT fkarmeA FOREIGN KEY(id_armeA) REFERENCES ARMES(id_arme);
+ALTER TABLE CLASSES ADD CONSTRAINT fkarmeB FOREIGN KEY(id_armeB) REFERENCES ARMES(id_arme);
+ALTER TABLE CLASSES ADD CONSTRAINT fkarmeC FOREIGN KEY(id_armeC) REFERENCES ARMES(id_arme);
+ALTER TABLE CLASSES ADD CONSTRAINT fkavatar FOREIGN KEY(id_avatar) REFERENCES AVATAR(id_avatar);
