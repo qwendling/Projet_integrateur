2c1f3c53334a759d8adf229b4a4678c4dad630cf
Sarah <sarah.murat@etu.unistra.fr>
2017-03-19 18:39:42 +0100

    script menu depart

---
+++ Unity_Project/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class GameMenu : MonoBehaviour {
+
+	public GameObject Menu;
+
+	public void OnStartClicked(){
+		Menu.SetActive(false);
+		SceneManager.LoadScene("Arena1");
+	}
+
+	public void EnterName(){
+		
+	}
+}
