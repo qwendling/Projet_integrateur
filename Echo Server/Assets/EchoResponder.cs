7a28df38a573a73800fd155f5039dadb0ae2e9fe
nsutter <suttercyrilsutternicolas@gmail.com>
2017-03-29 16:50:58 +0200

    [Create] projet unity serveur

---
+++ Echo Server/Assets/EchoResponder.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine.Networking;
+using UnityEngine.Networking.NetworkSystem;
+using UnityEngine;
+using GameMessages;
+
+public class EchoResponder : MonoBehaviour 
+{
+	NetworkManager netmgr;
+
+	public String getServerInfo()
+	{
+		return ("[ server address: " + netmgr.networkAddress + ", port number: " + netmgr.networkPort + " ]");
+	}
+	
+	public void sendSystemMessage(string deviceId, int connectionId, int content)
+	{
+		SystemMessage msg = new SystemMessage ();
+		msg.deviceId = deviceId;
+		msg.clientConnection = connectionId;
+		msg.content = content;
+
+		NetworkServer.SendToClient (connectionId, 100, msg);
+	}
+
+	public void onSystemMessage(NetworkMessage net)
+	{
+		SystemMessage sysmsg = net.ReadMessage<SystemMessage> ();
+		print ("SYSMSG RECEIVED [LEAP DEVICE ID: " + sysmsg.deviceId + ", CLIENT CONNECTION ID: " + sysmsg.clientConnection + ", CONTENT: " + sysmsg.content + "]");
+		if (sysmsg.content == MessageTypes.ASK_FOR_CONNECTION) 
+		{
+			//Create database entry with device link
+			//Confirm
+			sendSystemMessage(sysmsg.deviceId, sysmsg.clientConnection, MessageTypes.LINK_ESTABLISHED);
+		}
+	}
+	
+
+	public void onGameMessage(NetworkMessage net)
+	{
+		DataMessage msg = net.ReadMessage<DataMessage> ();
+		print ("DataMessage[ LEAP DEVICE ID: " + msg.deviceId + ", CODE MOUVEMENT: " + msg.mouvement + " ]");
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		//Set Network Manager
+		netmgr = gameObject.GetComponent<NetworkManager>();
+
+		//Configure Network Manager parameters
+		netmgr.networkAddress = "localhost";
+		netmgr.networkPort = 7000;
+
+		print("Server was configured to: " + getServerInfo());
+
+		netmgr.StartServer();
+
+		NetworkServer.RegisterHandler (200, onGameMessage);
+		NetworkServer.RegisterHandler (100, onSystemMessage);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
