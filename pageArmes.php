578cb8710a0b82e539534d4c87cf0ff11985d3b8
Jean-Philippe Abegg <abegg.jp@hotmail.fr>
2017-04-05 13:21:09 +0200

    [UPDATE] pages PHP pour la BDD
    Mise a jour des pages PHP pour la BDD. Les pages font un echo du contenu de la base

---
+++ pageArmes.php
@@ -0,0 +1,12 @@
+<?php
+	include "config.php";
+	
+	$requete = $dbCon->prepare("SELECT * FROM ARMES");
+	$requete->execute();
+	$donnees=$requete->fetchAll();
+    	$requete->closeCursor();
+	foreach ($donnees as $row) {
+		echo $row['id_arme'].",".$row['nom_arme'].",".$row['cadence_arme'].",".$row['degat_arme'].",".$row['range_arme'].';';
+	}
+	
+	
