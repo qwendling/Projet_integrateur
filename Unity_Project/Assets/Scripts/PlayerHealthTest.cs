7b5a389b92df1e31c21cdc2a1c4cc1ea5073e25b
Azranoth <awasmer96@yahoo.fr>
2017-04-12 16:57:49 +0200

    Players now respawn "randomly" when dying
    May require debug

---
+++ Unity_Project/Assets/Scripts/PlayerHealthTest.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class PlayerHealthTest : NetworkBehaviour {
+
+	public const int MAX_HEALTH = 100;
+
+	[SyncVar(hook = "OnChangeHealth")]
+	public int currentHealth = MAX_HEALTH;
+
+	public RectTransform healthBar;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+
+	public void TakeDamage(int amount) {
+		if (!isServer)
+			return;
+
+		currentHealth -= amount;
+		if (currentHealth <= 0) {
+			currentHealth = MAX_HEALTH;
+			RpcRespawn ();
+		}
+	}
+
+	void OnChangeHealth (int health) {
+		if (healthBar == null)
+			return;
+		healthBar.sizeDelta = new Vector2 (currentHealth, healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn() {
+		if (isLocalPlayer) {
+			//TODO
+				// Score dead /2
+				// Score killer++
+			this.GetComponent<PlayerSpawn>().RandomSpawn();
+		}
+	}
+}
+
