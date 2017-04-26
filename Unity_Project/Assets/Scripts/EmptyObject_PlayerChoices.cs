f70ca4426872086c262b8b981489a5170d97ffe4
Azranoth <awasmer96@yahoo.fr>
2017-04-26 15:40:54 +0200

    Rename script PlayerChoices pour la lisibilté de Martin

--- Unity_Project/Assets/Scripts/EmptyObject_PlayerChoices.cs
+++ Unity_Project/Assets/Scripts/EmptyObject_PlayerChoices.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerChoices : MonoBehaviour {
+public class EmptyObject_PlayerChoices : MonoBehaviour {
 
 	public GameObject _Model;
 	public GameObject _Wep1;
