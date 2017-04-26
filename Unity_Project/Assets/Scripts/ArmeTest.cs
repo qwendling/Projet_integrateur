e454b97f6da33a4e1435bc981b15b0402ba97aad
Azranoth <awasmer96@yahoo.fr>
2017-04-26 16:25:29 +0200

    Implémentation tir dans Arena1
    (le merge c'est ok)

---
+++ Unity_Project/Assets/Scripts/ArmeTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ArmeTest : Weapon {
+
+	public GameObject projectile;
+	public Color couleurTir;
+	public AudioSource Bruit;
+
+	// Use this for initialization
+	void Start () {
+		Bruit.enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public override void Shoot()
+	{
+		GameObject bullet = Instantiate(projectile, FireSpot.transform.position, FireSpot.transform.rotation) as GameObject;
+		bullet.GetComponent<MeshRenderer>().material.color = couleurTir;
+		bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 1000);
+		Destroy (bullet, 2.0f);
+	}
+}
---
+++ Unity_Project/Assets/Scripts/WeaponTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponTest : MonoBehaviour {
+
+	public int degat;
+	public float cadence;
+	public string nom;
+	public GameObject FireSpot;
+	public float vitesse = 1;
+
+	/*public float timeBetweenTicks = 2;
+	public float _timer = 2;*/
+	// Use this for initialization
+	void Start () {
+		GameObject _Player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		/*if(CLICK GAUCHE GET DOWN){
+			if(_timer <= 0){
+				//shoot
+				_timer = timeBetweenTicks;
+			}
+			else{
+				_timer -= 1.0*Time.deltaTime;
+			}
+		}
+		_timer = timeBetweenTicks;*/
+	}
+
+	public virtual void Shoot()
+	{
+		print("Tir de : " + nom);
+	}
+}
