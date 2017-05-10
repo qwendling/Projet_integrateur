89408eee78fb8e03fdad65d21230a006831504ad
Projet Intégrateur <projet@localhost.localdomain>
2017-05-10 14:12:42 +0200

    UPDATE: page arme

--- Script/pageArmes.php
+++ Script/pageArmes.php
@@ -6,7 +6,7 @@
 	$donnees=$requete->fetchAll();
     	$requete->closeCursor();
 	foreach ($donnees as $row) {
-		echo $row['id_arme'].",".$row['nom_arme'].",".$row['cadence_arme'].",".$row['degat_arme'].",".$row['range_arme'].';';
+		echo $row['id_arme'].",".$row['nom_arme'].",".$row['cadence_arme'].",".$row['degat_arme'].",".$row['range_arme'].",".$row['surchauffe_arme'].';';
 	}
 	
 	
