4cff393b32e7dc2540e54eab6fd8d5103176a6cd
pcatalina <pavlovcatalina@gmail.com>
2017-05-10 15:59:25 +0200

    fix radar area show problem

--- Unity_Project/Assets/Scripts/Radar.cs
+++ Unity_Project/Assets/Scripts/Radar.cs
@@ -1,20 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Radar : MonoBehaviour {
+public class Radar : NetworkBehaviour {
 
 	public GameObject radarPrefab;
 	public GameObject player;
+	public GameObject radarArena;
 	private float offset = 5.0f;
 
 	void Start () {
+		if(isLocalPlayer){
+			radarArena.SetActive (true);
+		}
+		else
+		{
+			radarArena.SetActive (false);
+		}
 		createRadarObjects();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// foreach(GameObject go in GameObject.FindGameObjectsWithTag("Player")) 
+		// {
+			// if(je joueur tire)
+			// {
+				// createRadarObjects();
+			// }
+		// }
 	}
 	
 	//player appears on radar for 5 seconds;
@@ -22,10 +37,10 @@ public class Radar : MonoBehaviour {
 	{
 		foreach(GameObject go in GameObject.FindGameObjectsWithTag("Player")) 
 		{
-
-			if(player.transform.position.y >= (go.transform.position.y - offset) && player.transform.position.y <= (go.transform.position.y + offset ))
+			if((player.transform.position.y >= (go.transform.position.y - offset) && player.transform.position.y <= (go.transform.position.y + offset ))&&(!isLocalPlayer))
 			{
-				Destroy(Instantiate(radarPrefab, go.transform.position, Quaternion.identity),5.0f);
+				// Destroy(Instantiate(radarPrefab, go.transform.position, Quaternion.identity),5.0f);
+				Instantiate(radarPrefab, go.transform.position, Quaternion.identity);
 			}
 		}
 	}
