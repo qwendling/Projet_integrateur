cea863aac2c77d57c2a34903d8f0f09b0f8bad03
Woap <ibis@etu.unistra.fr>
2017-05-11 18:41:41 +0200

    Transition automatique vers le jeu après le ACK

--- Unity_Project/Assets/Scripts/MenuManager_LeapChoice.cs
+++ Unity_Project/Assets/Scripts/MenuManager_LeapChoice.cs
@@ -7,17 +7,16 @@ public class MenuManager_LeapChoice : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	public void OnNextClicked(){
 
-		SceneManager.LoadScene ("Test_multi");
-		Cursor.visible = false;
+		
 	}
 }
